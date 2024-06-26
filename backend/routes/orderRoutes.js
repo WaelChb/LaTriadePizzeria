bc56b7c4b04a791f42c55ca4d0577e08e699d78e
WaelChb <144222573+WaelChb@users.noreply.github.com>
2024-06-27 01:40:10 +0200

    Update .gitignore

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const orderController = require("../controllers/orderController");
+const Order = require("../models/order");
 
 router.post("/", orderController.addOrder);
+
+// Route pour récupérer les commandes "en cours"
+router.get("/pending", async (req, res) => {
+  try {
+    const orders = await Order.find({ status: "en cours" });
+    res.json(orders);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 module.exports = router;
